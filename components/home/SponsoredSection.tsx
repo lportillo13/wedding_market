abbd38a45f42216b32edac2965a29046ddaee82e
Leonardo Portillo <lportillo@icnd.net>
2026-07-16 13:54:07 -0400

    Polish homepage section transitions

--- components/home/SponsoredSection.tsx
+++ components/home/SponsoredSection.tsx
@@ -10,8 +10,10 @@ export default function SponsoredSection({ hero, categories }: SponsoredSectionP
   const keywords = categories.items.flatMap((item) => [item.slug, item.label]);
 
   return (
-    <section className="container wm-home-premium-sponsored" id="about">
-      <ContextualSponsoredUnits pageKey="home" headline={hero.title} keywords={keywords} />
+    <section className="wm-home-premium-sponsored" id="about">
+      <div className="container">
+        <ContextualSponsoredUnits pageKey="home" headline={hero.title} keywords={keywords} />
+      </div>
     </section>
   );
 }
